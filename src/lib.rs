b9ee9538e5ab78758b18a7de54820cb7f66878cd
Andy Saul <andycsaul@gmail.com>
2021-02-01 11:59:00 +0000

    Rename tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,12 @@ pub fn fizzbuzz(n: u32) -> String {
 mod tests {
 
     #[test]
-    fn fizbuzz_1_returns_1_as_a_string() {
+    fn fizbuzz_1_prints_1() {
         assert_eq!("1", crate::fizzbuzz(1));
     }
 
     #[test]
-    fn fizbuzz_2_returns_2_as_a_string() {
+    fn fizbuzz_2_prints_2() {
         assert_eq!("2", crate::fizzbuzz(2));
     }
 }
