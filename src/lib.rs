8c5cb14783509c52fd5ad8f411db3878653d908f
Andy Saul <andycsaul@gmail.com>
2021-02-01 12:12:39 +0000

    Add some docs & doctests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+/// # fizzbuzz
+/// 
+/// Prints the fizzbuzz value of the given number:
+/// * 'Fizz' if the number is divisible by 3
+/// * 'Buzz' if the number is divisible by 5
+/// * 'FizzBuzz' if the number is divisible by 15
+/// * Prints the numerical value if not divisible by 3 or 5
+/// ```
+/// assert_eq!("1", fizzbuzz::fizzbuzz(1))
+/// ```
 pub fn fizzbuzz(n: u32) -> String {
     n.to_string()
 }
