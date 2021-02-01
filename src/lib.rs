475396e566446d3bad78d9669de0c89ae1677b57
Andy Saul <andycsaul@gmail.com>
2021-02-01 12:00:36 +0000

    Move tests into their own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,3 @@
 pub fn fizzbuzz(n: u32) -> String {
     n.to_string()
 }
-
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn fizbuzz_1_prints_1() {
-        assert_eq!("1", crate::fizzbuzz(1));
-    }
-
-    #[test]
-    fn fizbuzz_2_prints_2() {
-        assert_eq!("2", crate::fizzbuzz(2));
-    }
-}
---
+++ src/tests.rs
@@ -0,0 +1,14 @@
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn fizbuzz_1_prints_1() {
+        assert_eq!("1", crate::fizzbuzz(1));
+    }
+
+    #[test]
+    fn fizbuzz_2_prints_2() {
+        assert_eq!("2", crate::fizzbuzz(2));
+    }
+}
