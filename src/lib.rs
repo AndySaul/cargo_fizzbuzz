b45ebeab0d9b7a7ecf86f1105c9c7c892e779c45
Andy Saul <andycsaul@gmail.com>
2021-02-01 11:54:03 +0000

    Test fizzbuzz(2)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-pub fn fizzbuzz(_n: u32) -> String {
-    "1".to_string()
+pub fn fizzbuzz(n: u32) -> String {
+    n.to_string()
 }
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn fizbuzz_1_returns_1_as_a_string() {
         assert_eq!("1", crate::fizzbuzz(1));
+    }
+
+    #[test]
+    fn fizbuzz_2_returns_2_as_a_string() {
+        assert_eq!("2", crate::fizzbuzz(2));
     }
 }
