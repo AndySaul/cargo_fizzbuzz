d58bf76cf108bed908a22c60e80235ef8739b09a
Andy Saul <andycsaul@gmail.com>
2021-02-01 17:03:06 +0000

    Add examples header before doc assertions

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 /// * 'Buzz' if the number is divisible by 5
 /// * 'FizzBuzz' if the number is divisible by 15
 /// * Prints the numerical value if not divisible by 3 or 5
+/// 
+/// # Examples
 /// ```
 /// # use fizzbuzz::fizzbuzz;
 /// assert_eq!(fizzbuzz(1), "1");
